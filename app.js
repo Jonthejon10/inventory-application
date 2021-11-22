5ec7b315e4f1adc3f4a8a1ba373ab234a221d1a7
Ionut Ianchis <i.ionutz50@gmail.com>
2021-11-22 12:29:52 +0200

    Heroku deployment issues test

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
+console.log('Database_URL', process.env.DATABASE_URL)
 require('dotenv').config()
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-const indexRouter = require('./routes/index');
-const catalogRouter = require('./routes/catalog');
-const genresRouter = require('./routes/genres');
+const indexRouter = require('./routes/index')
+const catalogRouter = require('./routes/catalog')
+const genresRouter = require('./routes/genres')
 const compression = require('compression')
 const helmet = require('helmet')
 
-const app = express();
+const app = express()
 
 app.use(helmet())
 
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = process.env.url
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+var mongoose = require('mongoose')
+var mongoDB = process.env.DATABASE_URL
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/catalog', catalogRouter);
+app.use('/', indexRouter)
+app.use('/catalog', catalogRouter)
 app.use('/genres', genresRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+	next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
+})
 
-module.exports = app;
+module.exports = app
